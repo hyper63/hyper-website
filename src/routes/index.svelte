69890e04a983e886d9a6956b99700f4c873da0d4
Tom Wilson <twilson63@gmail.com>
2021-04-14 17:28:56 -0400

    update site

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,41 +4,47 @@
   import Button from '$lib/button.svelte'
   import Play from '$lib/play-button.svelte'
   import LinkButton from '$lib/link-button.svelte'
+  import Ports from '$lib/ports.svelte'
+  import FAQ from '$lib/faq.svelte'
+  import All from '$lib/all-n-one.svelte'
+  import Testimonials from '$lib/testimonials.svelte'
+  import Flow from '$lib/flow.svelte'
+  import Footer from '$lib/footer.svelte'
+
   let show = false
 </script>
 <svelte:head>
   <title>hyper.io - the service framework</title>
 </svelte:head>
 <Header />
-<main class="ml-24">
-  <section class="splash bg-no-repeat">
-    <div class="mt-20 w-1/2">
-      <h1 class="width-full height-full">Build Your Web Applications at <b>hyper</b> Speed</h1>
-      <p class="mt-12 w-3/4">Let your development team focus on creating applications that solve the problems of the business, 
+<main class="ml-0 pl-0 pr-0 mr-0">
+  <Flow />
+  <section class="splash bg-no-repeat pl-4 md:pl-24 md:pr-24">
+    <div class="mt-8 md:mt-20 md:w-1/2">
+      <h1 class="text-4xl font-semibold md:text-6xl width-full height-full">Build Your Web Applications at <b>hyper</b> Speed</h1>
+      <p class="text-md mt-4 mr-8 md:mt-12 md:w-3/4">Let your development team focus on creating applications that solve the problems of the business, 
       let hyper solve the problems of the cloud.</p>
-      <div class="mt-16 flex space-x-20">
+      <div class="mt-16 flex space-x-4 md:space-x-20">
         <Button bgColor="blue" txtColor="white">Get Started</Button>
         <Play>Watch</Play>
       </div>
     </div> 
   </section>
-  <section class="flex w-full">
-    <div class="w-1/2">
-    <h5 class="mt-72">THE PROBLEM</h5>
-    <h2 class="mt-16">Software can cause just as many problems as it solves</h2>
-    <p class="mt-16">
-    Unfortunately, often times, the result leads to disappointment, As a software development community, can we do better? Can we create products that are a joy, to not only build, but to maintain, over time? Product that can migrate to new technologies and platforms incrementally?
-    </p>
-    </div>
-    <div class="w-1/2 ml-44 mt-44 mr-44">
-      <img src="/flow1.svg" alt="software-problem" />
+  <img class="md:hidden" src="/homepage-splash.svg" />
+  <section class="flex w-full md:pt-44 pl-4 md:pl-24 md:pr-24">
+    <div class="md:w-1/2 2xl:w-1/3">
+      <h5 class="">THE PROBLEM</h5>
+      <h2 class="text-3xl font-semibold md:text-5xl md:mt-16">Software can cause just as many problems as it solves</h2>
+      <p class="mt-4 md:mt-16">
+      Unfortunately, often times, the result leads to disappointment, As a software development community, can we do better? Can we create products that are a joy, to not only build, but to maintain, over time? Product that can migrate to new technologies and platforms incrementally?
+      </p>
     </div>
   </section>
-  <img class="connectors" src="/connectors.svg" alt="connectors" />
-  <section class="flex w-full">
-    <div class="w-1/2">
+  <img class="md:hidden" src="/flow1.svg" />
+  <section class="flex w-full pl-4 md:pt-44 md:pl-24 md:pr-24">
+    <div class="md:w-1/2 2xl:w-1/3">
       <h5>OUR SOLUTION</h5>
-      <h2 class="mt-8">
+      <h2 class="text-3xl font-semibold md:text-5xl md:mt-8">
       Don't re-invent the wheel
       <br />
       Use a service framework.
@@ -50,21 +56,53 @@
       <LinkButton styles="w-max" bgColor="lightblue" txtColor="blue" href="/company">Learn More About Us</LinkButton>
       </div>
     </div>
-    <div class="pl-20">
-      <img style="margin-top: -14px;" src="/hyper-box.svg" alt="hyper" />
+  </section>
+  <div class="mt-16 md:hidden flex items-center justify-center">
+  <img class="" src="/hyper-box.svg" />
+  </div>
+  <Ports styles="pl-24 pr-24" />
+  <All styles="pl-24 pr-24" />
+  <section class="flex flex-col bg-whitesmoke items-center mt-4 pt-8 md:mt-16 md:pt-8 md:pl-24 md:pr-24">
+    <h1 class="text-3xl md:text-6xl">FAQs</h1>
+    <p class="px-4 my-4 text-base md:mt-16 md:mb-8">Can't find the answer you're looking for? Reach out to our <a href="/contact" class="text-red underline">customer support team</a>.</p>
+    <div class="flex flex-col w-3/4 md:w-2/3">
+    <FAQ styles="w-full mb-8 bg-white">
+      <span slot="question">What is hyper?</span>
+      <span slot="answer">A service framework</span>
+    </FAQ>
+    <FAQ styles="w-full mb-8 bg-white">
+      <span slot="question">What is hyper?</span>
+      <span slot="answer">A service framework</span>
+    </FAQ>
+    <FAQ styles="w-full mb-8 bg-white">
+      <span slot="question">What is hyper?</span>
+      <span slot="answer">A service framework</span>
+    </FAQ>
+    <FAQ styles="w-full mb-8 bg-white">
+      <span slot="question">What is hyper?</span>
+      <span slot="answer">A service framework</span>
+    </FAQ>
+    <FAQ styles="w-full mb-8 bg-white">
+      <span slot="question">What is hyper?</span>
+      <span slot="answer">A service framework</span>
+    </FAQ>
     </div>
   </section>
-  <img class="circle" src="/circle.svg" alt="circle" />
-  <img class="line" src="/line.svg" alt="line" />
+  <Testimonials styles="pl-24 pr-24" />  
 </main>
+<Footer />
 <style lang="postcss">
-  main {
-    min-width: 1440px;
-    width: 1440px;
+  @media (min-width: 768px) {
+    .splash {
+      background-image: url('/homepage-splash.svg');
+      height: 950px;
+    }
   }
-  .splash {
-    background-image: url('/homepage-splash.svg');
-    height: 950px;
+  main {
+    width: 100%;
+  }
+  .flow {
+    @apply sm:hidden md:block absolute opacity-90;
   }
   .connectors {
     @apply absolute;
---
+++ src/routes/pricing.svelte
@@ -0,0 +1,78 @@
+<script>
+  import Header from '$lib/header.svelte'
+  import Footer from '$lib/footer.svelte'
+</script>
+<Header />
+<main>
+  <h1 class="text-3xl pl-4 mt-8 mb-8">Simple <b>Pricing</b></h1>
+  <section class="space-y-4">
+    <article class="mx-4 border-3 border-lightgray px-4 py-4 rounded-lg">
+      <header>
+        <button class="bg-blue text-white float-right">Go</button>
+        <h3 class="text-xl">Developer</h3>
+        <h5 class="text-lg text-black font-bold">Free</h5>
+      </header>
+      <p class="mt-8 text-base text-darkgray">Multi-Tenant Cloud with access to hyper slack channel</p>
+    </article>
+    <article class="mx-4 border-3 border-lightgray px-4 py-4 rounded-lg">
+      <header>
+        <button class="bg-blue text-white float-right">Go</button>
+        <h3 class="text-xl">Startup</h3>
+        <h5 class="text-lg text-black font-bold">$500/mo</h5>
+      </header>
+      <p class="mt-8 text-base text-darkgray">1 dedicated hyper cloud service</p>
+    </article>
+    <article class="mx-4 border-3 border-lightgray px-4 py-4 rounded-lg">
+      <header>
+        <button class="bg-blue text-white float-right">Go</button>
+        <h3 class="text-xl">Agency</h3>
+        <h5 class="text-lg text-black font-bold">$2000/mo</h5>
+        <div class="my-4">
+        <span class="p-2 border-3 border-blue text-blue rounded-full">
+          Save $500
+        </span>
+        </div>
+      </header>
+      <p class="mt-8 text-base text-darkgray">5 dedicated hyper cloud services</p>
+    </article>
+    <article class="mx-4 border-3 border-lightgray px-4 py-4 rounded-lg">
+      <header>
+        <h3 class="text-xl">Enterprise</h3>
+        <h5 class="text-lg text-black font-bold">Contact Us</h5>
+      </header>
+      <p class="mt-8 text-base text-darkgray">On premise hyper cloud service</p>
+      <button class="my-4 bg-lightblue text-blue w-full flex items-center justify-center">Contact</button>
+    </article>
+  </section>
+  <h5 class="mx-4 mt-8 mb-4 uppercase">All Paid Plans Include:</h5>
+  <ul class="mx-4">
+    <li class="flex items-center">
+      <img src="/data-sm.svg" />
+      <p class="text-sm font-semibold">Data Service</p>
+    </li>
+    <li class="flex items-center">
+      <img src="/search-sm.svg" />
+      <p class="text-sm font-semibold">Search Service</p>
+    </li>
+    <li class="flex items-center">
+      <img src="/cache-sm.svg" />
+      <p class="text-sm font-semibold">Cache Service</p>
+    </li>
+    <li class="flex items-center">
+      <img src="/storage-sm.svg" />
+      <p class="text-sm font-semibold">Storage Service</p>
+    </li>
+    <li class="flex items-center">
+      <img src="/queue-sm.svg" />
+      <p class="text-sm font-semibold">Queue Service</p>
+    </li>
+  </ul>
+  <hr class="mx-4 my-8 border-lightgray" />
+  <section class="mx-4">
+    <p class="flex space-x-4"><img src="/check.svg" /><span>Daily backups</span></p>
+    <p class="flex space-x-4"><img src="/check.svg" /><span>24/7 Monitoring</span></p>
+    <p class="flex space-x-4" ><img src="/check.svg" /><span>Hosted or Managed Solution</span></p>
+    <p class="flex space-x-4"><img src="/check.svg" /><span>Dedicated sub-domain name</span></p>
+  </section>
+</main>
+<Footer />
