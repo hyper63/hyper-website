39950bd011b387fcf8f500f60c51c7419ec7428f
Tom Wilson <twilson63@gmail.com>
2021-04-15 15:59:15 -0400

    update mobile pages

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,6 +1,16 @@
 <script>
 import { goto } from '$app/navigation'
 import NavButton from '$lib/nav-button.svelte'
+
+let mobileMenu = false
+
+function mobile() {
+  mobileMenu = !mobileMenu
+}
+
+function isCurrentPage(ref) {
+  return window.location.pathname === ref ? 'active' : 'link'
+}
 </script>
 <header class="w-full flex justify-between mt-4 ml-4 md:mt-20 md:ml-24 md:mr-44">
   <section class="flex items-center width-full">
@@ -9,12 +19,14 @@ import NavButton from '$lib/nav-button.svelte'
     <!-- name -->
     <h3 class="ml-3">hyper</h3>
   </section>
-  <button class="md:hidden border-0 mr-4">
-    <svg width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M0 1H30M30 8.5H0M0 16H30" stroke="#1286F6" stroke-width="2"/>
-    </svg>
-  </button>
-  <nav class="hidden md:block flex items-center space-x-4">
+  <section class="md:hidden">
+    <button class="border-0 mr-4 focus:outline-none" on:click={() => mobile()}>
+      <svg width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 1H30M30 8.5H0M0 16H30" stroke="#1286F6" stroke-width="2"/>
+      </svg>
+    </button>
+  </section>
+  <nav class="hidden w-1/2 md:block flex items-center space-x-4">
     <!-- product -->
     <a class="font-space uppercase text-sm" href="/product">product</a>
     <!-- faq -->
@@ -28,4 +40,29 @@ import NavButton from '$lib/nav-button.svelte'
     <!-- api docs button -->
     <NavButton on:click={() => goto('https://docs.hyper63.com')}>API DOCS</NavButton>
   </nav>
+
 </header>
+{#if mobileMenu}
+<div class="sm:hidden mx-4" id="mobile-menu">
+  <div class="pt-2 pb-3 space-y-1">
+    <a href="/" class="{isCurrentPage('/')}" aria-current="page">Home</a>
+    <a href="/product" class="{isCurrentPage('/product')}">Product</a>
+    <a href="/pricing" class="{isCurrentPage('/pricing')}">Pricing</a>
+    <a href="/faq" class="{isCurrentPage('/faq')}">FAQ</a>
+    <a href="/company" class="{isCurrentPage('/company')}">Company</a>
+    <a href="https://blog.hyper63.com" class="link">Blog</a>
+    <a href="/get-started" class="{isCurrentPage('/get-started')}">Get Started</a>
+    <hr />
+    <a href="https://docs.hyper63.com" class="link">API DOCS</a>
+  </div>
+</div>
+{/if}
+
+<style lang="postcss">
+  .active {
+    @apply bg-lightgreen border-green text-green block pl-3 pr-4 py-2 border-l-4 text-base font-medium;
+  }
+  .link {
+    @apply border-transparent text-gray hover:bg-whitesmoke hover:border-gray hover:text-darkgray block pl-3 pr-4 py-2 border-l-4 text-base font-medium; 
+  }
+</style>
---
+++ src/routes/company.svelte
@@ -0,0 +1,50 @@
+<script>
+  import Header from '$lib/header.svelte'
+  import Footer from '$lib/footer.svelte'
+  import Button from '$lib/button.svelte'
+</script>
+<Header />
+<main>
+  <section class="w-full flex px-4 pt-8">
+    <h1 class="text-5xl mt-8">About <br /><b>hyper</b></h1>
+    <div class="w-full flex items-center justify-center">
+    <img src="/company-bolt.svg" alt="company bolt logo" />
+    </div>
+  </section>
+  <section>
+    <p class="px-4 text-base text-darkgray">hyper, is a company focused on building products and services
+    for developers and improving the development experience. As a result,
+    development teams are able to grow into high performing teams and create 
+    incredible products.
+    </p>
+    <Button styles="mx-4 mt-8">Get Started</Button>
+  </section>
+  <img class="mt-8 mx-auto" src="/swirl.svg" alt="swirl" />
+  <section class="pt-8 px-4">
+    <h2 class="text-3xl">Software is <br />eating the world</h2>
+    <p class="mt-4 text-darkgray text-base">but one can argule that software can cause just as many problems as it is meant to solve.</p>
+    <p class="mt-8 text-darkgray text-base">Building software is more than building a product and then selling it. It is a constant, fluid, process of discovery and refinement, a process that necessitates nurture and focus. Unfortunately, often times, the result leads to disappointment. As a software development community, can we do better? Can we create products that are a joy, to not only build, but to maintain, over time? Products that can migrate to new technologies and platforms, incrementally?</p>
+  </section>
+  <section class="px-4 pt-8">
+    <h3 class="text-2xl text-center">To that end, all software applications should be built with the following requirements:</h3>
+    <div class="mt-8 flex justify-center">
+      <img src="/extensibility.svg" alt="extensibility image" />
+    </div>
+    <div class="mt-8 flex justify-center">
+      <img src="/scalability.svg" alt="extensibility image" />
+    </div>
+    <div class="mt-8 flex justify-center">
+      <img src="/flexibility.svg" alt="extensibility image" />
+    </div>
+  </section>
+  <section class="px-4 pt-8">
+    <h3 class="text-2xl">We're here <br /><b>to help</b></h3>
+    <p class="mt-4 text-darkgray text-base">
+We are building a new set of products and services that have the potential to transform the software community. These products and services provide development teams the guidance and capabilities to, not only, highly perform when building software, but to future proof their products, and create constant and consistent value over time.
+    </p>
+    <Button styles="mt-8">Get Started</Button>
+  </section>
+  <img class="mt-8" src="/hyper-connected.svg" alt="connected" />
+</main>
+
+<Footer />
