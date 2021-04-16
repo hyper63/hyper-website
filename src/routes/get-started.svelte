b158737c1ea0373e03b46222d05986e2cab559dc
Tom Wilson <twilson63@gmail.com>
2021-04-16 13:49:30 +0000

    added getting started m page

---
+++ src/routes/get-started.svelte
@@ -0,0 +1,82 @@
+<script>
+  import Header from "$lib/header.svelte";
+  import Footer from "$lib/footer.svelte";
+  import Select from "svelte-select";
+
+  let items = [
+    { value: "developer", label: "Developer" },
+    { value: "startup", label: "Startup" },
+    { value: "agency", label: "Agency" },
+  ];
+
+  let selectedValue = { value: "developer", label: "Developer" };
+
+  function handleSelect(event) {
+    console.log("selected item", event.detail);
+    // .. do something here 🙂
+  }
+</script>
+<Header />
+<main>
+  <h1 class="mx-4 my-8 text-center text-5xl">Get <b>Started</b></h1>
+  <section class="mx-4 bg-whitesmoke rounded-lg p-4">
+    <p>Step 1 of 4: Choose your pricing plan</p>
+    <div class="select">
+      <Select {items} {selectedValue} on:select={handleSelect} />
+    </div>
+    <div class="mt-4 p-8 rounded-lg border-3 border-lightgray">
+      <h3>Developer</h3>
+      <h4 class="font-semibold">Free</h4>
+      <p class="text-darkgray">1 hyper app service</p>
+      <div class="mt-8 space-y-4">
+        <article class="flex items-center">
+          <img src="/data-sm.svg" />
+          <span>Data Service</span>
+        </article>
+        <article class="flex items-center">
+          <img src="/search-sm.svg" />
+          <span>Search Service</span>
+        </article>
+        <article class="flex items-center">
+          <img src="/cache-sm.svg" />
+          <span>Cache Service</span>
+        </article>
+        <article class="flex items-center">
+          <img src="/storage-sm.svg" />
+          <span>Storage Service</span>
+        </article>
+        <article class="flex items-center">
+          <img src="/queue-sm.svg" />
+          <span>Queue Service</span>
+        </article>
+      </div>
+    </div>
+    <div class="mt-4 p-8 space-y-2">
+      <div class="flex items-center">
+        <img src="/check.svg" />
+        <span class="ml-4">Multi-Tenant Service</span>
+      </div>
+      <div class="flex items-center">
+        <img src="/check.svg" />
+        <span class="ml-4">Daily Backups</span>
+      </div>
+      <div class="flex items-center">
+        <img src="/check.svg" />
+        <span class="ml-4">Community Chat Support</span>
+      </div>
+    </div>
+  </section>
+</main>
+<Footer />
+<style lang="postcss">
+  .select {
+    --border: 3px solid #c6cad3;
+    --borderRadius: 12px;
+    --placeholderColor: #1286f6;
+    @apply font-space text-xl;
+    margin: 0;
+    padding: 0;
+    border: none;
+  }
+</style>
+
--- src/routes/style.svelte
+++ src/routes/style.svelte
@@ -1,60 +1,91 @@
 <script>
-import Color from '$lib/color.svelte'
-import Button from '$lib/button.svelte'
-import Play from '$lib/play-button.svelte'
-import NavButton from '$lib/nav-button.svelte'
-import LinkBtn from '$lib/link-button.svelte'
-import Link from '$lib/link.svelte'
-import Pagination from '$lib/pagination.svelte'
-import FAQ from '$lib/faq.svelte'
-import Input from '$lib/input.svelte'
-import Search from '$lib/search.svelte'
-import Select from 'svelte-select'
+  import Color from "$lib/color.svelte";
+  import Button from "$lib/button.svelte";
+  import Play from "$lib/play-button.svelte";
+  import NavButton from "$lib/nav-button.svelte";
+  import LinkBtn from "$lib/link-button.svelte";
+  import Link from "$lib/link.svelte";
+  import Pagination from "$lib/pagination.svelte";
+  import FAQ from "$lib/faq.svelte";
+  import Input from "$lib/input.svelte";
+  import Search from "$lib/search.svelte";
+  import Select from "svelte-select";
 
-let show = false
+  let show = false;
 
   let items = [
-    {value: 'chocolate', label: 'Chocolate'},
-    {value: 'pizza', label: 'Pizza'},
-    {value: 'cake', label: 'Cake'},
-    {value: 'chips', label: 'Chips'},
-    {value: 'ice-cream', label: 'Ice Cream'},
+    { value: "chocolate", label: "Chocolate" },
+    { value: "pizza", label: "Pizza" },
+    { value: "cake", label: "Cake" },
+    { value: "chips", label: "Chips" },
+    { value: "ice-cream", label: "Ice Cream" },
   ];
 
-  let selectedValue = {value: 'cake', label: 'Cake'};
+  let selectedValue = { value: "cake", label: "Cake" };
 
   function handleSelect(event) {
-    console.log('selected item', event.detail);
+    console.log("selected item", event.detail);
     // .. do something here 🙂
   }
-
 </script>
+
+<style lang="postcss">
+  .select {
+    --border: 3px solid #c6cad3;
+    --borderRadius: 12px;
+    --placeholderColor: #1286f6;
+    @apply font-space text-xl;
+    margin: 0;
+    padding: 0;
+    border: none;
+  }
+  .box {
+    @apply rounded;
+    height: 38px;
+    width: 38px;
+    margin: 4px;
+  }
+  .colors {
+    margin-top: 24px;
+  }
+
+  p {
+    margin-bottom: 16px;
+  }
+</style>
+
 <main class="container mx-auto mt-5">
   <section class="p-20">
     <h2 class="mb-8">Typography</h2>
     <h1>H1 Example Text and <b>Bold</b></h1>
     <h1 class="mb-8">Two Lines</h1>
-    
+
     <h2>H2/ Example Text with</h2>
     <h2 class="mb-8">two lines to show lineheight.</h2>
 
     <h3>H3/ Example Text with two lines</h3>
     <h3 class="mb-8">to show lineheight</h3>
-    
+
     <h4>H4/ Example Text with two lines</h4>
     <h4>hyper is awesome</h4>
     <h4 class="mb-8">Add more lines</h4>
-   
 
     <h5 class="mb-8">H5/ Example Text</h5>
 
-    <blockquote class="mb-8">// Blockquote Example Software development
-      is the constant embrace. The secret of the universe is 42</blockquote>
+    <blockquote class="mb-8">
+      // Blockquote Example Software development is the constant embrace. The
+      secret of the universe is 42
+    </blockquote>
 
-    <p>A simple paragraph, core basic paragraph need more text, for this block so that it can show how things are working</p>
+    <p>
+      A simple paragraph, core basic paragraph need more text, for this block so
+      that it can show how things are working
+    </p>
 
-    <p class="detail">A detailed paragraph that should show up with a 600 weight</p>
-    
+    <p class="detail">
+      A detailed paragraph that should show up with a 600 weight
+    </p>
+
     <h2 class="mt-8 mb-10">Colors</h2>
     <div class="colors flex justify-between">
       <Color color="black" />
@@ -64,7 +95,7 @@ let show = false
       <Color color="blue" />
       <Color color="purple" />
       <Color color="green" />
-    </div>  
+    </div>
     <div class="colors flex justify-between">
       <Color color="darkgray" />
       <Color color="lightyellow" />
@@ -114,15 +145,25 @@ let show = false
         </code>
       </pre>
     </div>
-    
+
     <h4 class="mt-8">Link Buttons</h4>
     <div class="mt-8 flex">
       <LinkBtn href="/">Learn More About Us</LinkBtn>
-      <LinkBtn href="/" styles="ml-3" bgColor="active-yellow">Learn More About Us</LinkBtn>
-      <LinkBtn href="/" styles="ml-3" bgColor="active-orange">Learn More About Us</LinkBtn>
-      <LinkBtn href="/" styles="ml-3" bgColor="active-red">Learn More About Us</LinkBtn>
-      <LinkBtn href="/" styles="ml-3" bgColor="active-purple">Learn More About Us</LinkBtn>
-      <LinkBtn href="/" styles="ml-3" bgColor="active-green">Learn More About Us</LinkBtn>
+      <LinkBtn href="/" styles="ml-3" bgColor="active-yellow">
+        Learn More About Us
+      </LinkBtn>
+      <LinkBtn href="/" styles="ml-3" bgColor="active-orange">
+        Learn More About Us
+      </LinkBtn>
+      <LinkBtn href="/" styles="ml-3" bgColor="active-red">
+        Learn More About Us
+      </LinkBtn>
+      <LinkBtn href="/" styles="ml-3" bgColor="active-purple">
+        Learn More About Us
+      </LinkBtn>
+      <LinkBtn href="/" styles="ml-3" bgColor="active-green">
+        Learn More About Us
+      </LinkBtn>
     </div>
     <div class="mt-3">
       <h5>code</h5>
@@ -135,19 +176,11 @@ let show = false
     <h4 class="mt-8">Play Buttons</h4>
     <div class="mt-3 flex justify-between">
       <Play>Watch</Play>
-      <Play bgColor="lightblue" iconColor="blue">
-        Watch
-      </Play>
+      <Play bgColor="lightblue" iconColor="blue">Watch</Play>
 
-      <Play bgColor="lightorange" iconColor="orange">
-        Watch
-      </Play>
-      <Play bgColor="lightred" iconColor="red">
-        Watch
-      </Play>
-      <Play bgColor="lightpurple" iconColor="purple">
-        Watch
-      </Play>
+      <Play bgColor="lightorange" iconColor="orange">Watch</Play>
+      <Play bgColor="lightred" iconColor="red">Watch</Play>
+      <Play bgColor="lightpurple" iconColor="purple">Watch</Play>
     </div>
     <div class="mt-3">
       <h5>code</h5>
@@ -160,12 +193,11 @@ let show = false
     <h4 class="mt-8">Nav Buttons</h4>
     <div class="mt-3 flex justify-between">
       <NavButton>API DOCS</NavButton>
-      <NavButton txtColor='yellow' bgColor='lightyellow'>API DOCS</NavButton>
-      <NavButton txtColor='orange' bgColor='lightorange'>API DOCS</NavButton>
-      <NavButton txtColor='red' bgColor='lightred'>API DOCS</NavButton>
-      <NavButton txtColor='purple' bgColor='lightpurple'>API DOCS</NavButton>
-      <NavButton txtColor='green' bgColor='lightgreen'>API DOCS</NavButton>
-
+      <NavButton txtColor="yellow" bgColor="lightyellow">API DOCS</NavButton>
+      <NavButton txtColor="orange" bgColor="lightorange">API DOCS</NavButton>
+      <NavButton txtColor="red" bgColor="lightred">API DOCS</NavButton>
+      <NavButton txtColor="purple" bgColor="lightpurple">API DOCS</NavButton>
+      <NavButton txtColor="green" bgColor="lightgreen">API DOCS</NavButton>
     </div>
     <div class="mt-3">
       <h5>code</h5>
@@ -186,36 +218,36 @@ let show = false
     </div>
     <h4 class="mt-8">Button Colors</h4>
     <div class="mt-3 flex">
-      <div class="bg-red box"></div>
-      <div class="bg-blue box"></div>
-      <div class="bg-yellow box"></div>
-      <div class="bg-orange box"></div>
-      <div class="bg-purple box"></div>
-      <div class="bg-green box"></div>
+      <div class="bg-red box" />
+      <div class="bg-blue box" />
+      <div class="bg-yellow box" />
+      <div class="bg-orange box" />
+      <div class="bg-purple box" />
+      <div class="bg-green box" />
     </div>
     <div class="mt-1 flex">
-      <div class="bg-gradient-red box"></div>
-      <div class="bg-gradient-blue box"></div>
-      <div class="bg-gradient-yellow box"></div>
-      <div class="bg-gradient-orange box"></div>
-      <div class="bg-gradient-purple box"></div>
-      <div class="bg-gradient-green box"></div>
+      <div class="bg-gradient-red box" />
+      <div class="bg-gradient-blue box" />
+      <div class="bg-gradient-yellow box" />
+      <div class="bg-gradient-orange box" />
+      <div class="bg-gradient-purple box" />
+      <div class="bg-gradient-green box" />
     </div>
     <div class="mt-1 flex">
-      <div class="bg-lightred box"></div>
-      <div class="bg-lightblue box"></div>
-      <div class="bg-lightyellow box"></div>
-      <div class="bg-lightorange box"></div>
-      <div class="bg-lightpurple box"></div>
-      <div class="bg-lightgreen box"></div>
+      <div class="bg-lightred box" />
+      <div class="bg-lightblue box" />
+      <div class="bg-lightyellow box" />
+      <div class="bg-lightorange box" />
+      <div class="bg-lightpurple box" />
+      <div class="bg-lightgreen box" />
     </div>
     <h4 class="mt-8">Pagination</h4>
-    <Pagination pages=5 /> 
-    <Pagination pages=5 color="blue" /> 
-    <Pagination pages=5 color="yellow" /> 
-    <Pagination pages=5 color="orange" /> 
-    <Pagination pages=5 color="purple" /> 
-    <Pagination pages=5 color="green" /> 
+    <Pagination pages="5" />
+    <Pagination pages="5" color="blue" />
+    <Pagination pages="5" color="yellow" />
+    <Pagination pages="5" color="orange" />
+    <Pagination pages="5" color="purple" />
+    <Pagination pages="5" color="green" />
     <h5>code</h5>
     <pre>
       <code>
@@ -225,7 +257,8 @@ let show = false
     <h3 class="mt-8">Elements</h3>
     <h4 class="mt-8">FAQ Toggle</h4>
     <FAQ class="mt-8">
-      <span slot="question">How can I know if the services hyper provides is right for me?</span>
+      <span slot="question">How can I know if the services hyper provides is
+        right for me?</span>
       <span slot="answer">All things are benefited greatly by hyper!</span>
     </FAQ>
     <h5>Code</h5>
@@ -249,63 +282,44 @@ let show = false
     I could try to copy to the src/lib dir and see if it works
     -->
     <div class="select">
-      <Select {items} {selectedValue} on:select={handleSelect}></Select>
+      <Select {items} {selectedValue} on:select={handleSelect} />
     </div>
   </section>
-  
 </main>
 <footer>
   {#if show}
-  <p>adding colors here so they do not get purged</p>
-  <p class="text-black bg-white">Colors</p>
-  <p class="text-darkgray bg-lightgray">Colors</p>
-  <p class="text-gray bg-black">Colors</p>
-  <p class="text-lightgray bg-darkgray">Colors</p>
-  <p class="text-whitesmoke bg-gray">Colors</p>
-  <p class="text-yellow bg-lightyellow hover:bg-yellow">Colors</p>
-  <p class="text-orange bg-lightorange hover:bg-orange">Colors</p>
-  <p class="text-red bg-lightred hover:bg-red">Colors</p>
-  <p class="text-blue bg-lightblue hover:bg-blue">Colors</p>
-  <p class="text-purple bg-lightpurple hover:bg-purple">Colors</p>
-  <p class="text-green bg-lightgreen hover:bg-green">Colors</p>
+    <p>adding colors here so they do not get purged</p>
+    <p class="text-black bg-white">Colors</p>
+    <p class="text-darkgray bg-lightgray">Colors</p>
+    <p class="text-gray bg-black">Colors</p>
+    <p class="text-lightgray bg-darkgray">Colors</p>
+    <p class="text-whitesmoke bg-gray">Colors</p>
+    <p class="text-yellow bg-lightyellow hover:bg-yellow">Colors</p>
+    <p class="text-orange bg-lightorange hover:bg-orange">Colors</p>
+    <p class="text-red bg-lightred hover:bg-red">Colors</p>
+    <p class="text-blue bg-lightblue hover:bg-blue">Colors</p>
+    <p class="text-purple bg-lightpurple hover:bg-purple">Colors</p>
+    <p class="text-green bg-lightgreen hover:bg-green">Colors</p>
 
-  <p class="text-lightyellow bg-yellow hover:bg-lightyellow">Colors</p>
-  <p class="text-lightorange bg-orange hover:bg-lightorange">Colors</p>
-  <p class="text-lightred bg-red hover:bg-lightred">Colors</p>
-  <p class="text-lightblue bg-blue hover:bg-lightblue">Colors</p>
-  <p class="text-lightpurple bg-purple hover:bg-lightpurple">Colors</p>
-  <p class="text-lightgreen bg-green hover:bg-lightgreen">Colors</p>
-  
-  <p class="text-active-yellow bg-active-yellow hover:bg-lightyellow">Colors</p>
-  <p class="text-active-orange bg-active-orange hover:bg-lightorange">Colors</p>
-  <p class="text-active-red bg-active-red hover:bg-lightred">Colors</p>
-  <p class="text-active-blue bg-active-blue hover:bg-lightblue">Colors</p>
-  <p class="text-active-purple bg-active-purple hover:bg-lightpurple">Colors</p>
-  <p class="text-active-green bg-active-green hover:bg-lightgreen">Colors</p>
-  <p class="border-3 text-white bg-white hover:bg-white">All White</p>
+    <p class="text-lightyellow bg-yellow hover:bg-lightyellow">Colors</p>
+    <p class="text-lightorange bg-orange hover:bg-lightorange">Colors</p>
+    <p class="text-lightred bg-red hover:bg-lightred">Colors</p>
+    <p class="text-lightblue bg-blue hover:bg-lightblue">Colors</p>
+    <p class="text-lightpurple bg-purple hover:bg-lightpurple">Colors</p>
+    <p class="text-lightgreen bg-green hover:bg-lightgreen">Colors</p>
+
+    <p class="text-active-yellow bg-active-yellow hover:bg-lightyellow">
+      Colors
+    </p>
+    <p class="text-active-orange bg-active-orange hover:bg-lightorange">
+      Colors
+    </p>
+    <p class="text-active-red bg-active-red hover:bg-lightred">Colors</p>
+    <p class="text-active-blue bg-active-blue hover:bg-lightblue">Colors</p>
+    <p class="text-active-purple bg-active-purple hover:bg-lightpurple">
+      Colors
+    </p>
+    <p class="text-active-green bg-active-green hover:bg-lightgreen">Colors</p>
+    <p class="border-3 text-white bg-white hover:bg-white">All White</p>
   {/if}
 </footer>
-<style lang="postcss">
-  .select {
-    --border: 3px solid #C6CAD3;
-    --borderRadius: 12px;
-    --placeholderColor: #1286F6;
-    @apply font-space text-xl;
-    margin: 0;
-    padding: 0;
-    border: none;
-  }
-  .box {
-    @apply rounded;
-    height: 38px;
-    width: 38px;
-    margin: 4px;
-  }
-  .colors {
-    margin-top: 24px;
-  }
-
-  p {
-    margin-bottom: 16px;
-  }
-</style>
