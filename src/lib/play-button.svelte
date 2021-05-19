cd71abaa1c68df8e37cebbec499f84b1de837d62
Tom Wilson <twilson63@gmail.com>
2021-05-19 12:08:00 +0000

    fixed play button watch label

--- src/lib/play-button.svelte
+++ src/lib/play-button.svelte
@@ -2,18 +2,19 @@
   export let txtColor = 'black'
   export let bgColor = 'whitesmoke'
   export let iconColor = '#7C7F86'
+  
 </script>
-<div class="flex items-center">
-  <button class="bg-whitesmoke text-{iconColor} hover:bg-{bgColor} rounded-full flex items-center justify-center" >
-    
-    <svg width="19" height="23" viewBox="0 0 19 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+<button class="m-0 p-0 flex space-x-2 focus:outline-none" >    
+  <div class="p-6 bg-whitesmoke text-{iconColor} hover:bg-{bgColor} rounded-full flex items-center justify-center">
+    <svg class="ml-2" width="19" height="23" viewBox="0 0 19 23" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M0 22.813V0.812988L19 11.313L0 22.813Z" fill="inherit"/>
     </svg>
-  </button>
-  <span class="font-mono font-bold text-xl text-black hover:text-{txtColor} ml-3">
+  </div> 
+  <span class="font-mono font-bold text-xl text-black hover:text-{txtColor}">
     <slot></slot>
   </span>
-</div>
+</button>
+  
 <style lang="postcss">
   div {
     width: 145px;
