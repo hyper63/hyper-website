a6593d51964a6609ab1fcfa042540e411dafd96d
tripott <tripott@gmail.com>
2021-05-19 14:21:00 +0000

    Button with composition.  All in one CTA button

--- src/lib/all-n-one.svelte
+++ src/lib/all-n-one.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Button from './button.svelte'
+  import Button from '$lib/button.svelte'
 
   export let styles = ''
 </script>
@@ -7,7 +7,10 @@
   <h5>HYPER POWER</h5>
   <h1 class="text-4xl mt-4 md:text-6xl md:mt-16">All-In-One</h1>
   <p class="mt-4 text-base md:text-2xl md:mt-12 md:w-1/2 text-center">hyper contains all of the services primitives to build applications without having to deal with servers, services or clouds.</p>
-  <Button styles="mt-16 mb-24">Get Started</Button>
+  <!-- <Button styles="mt-16 mb-24">Get Started</Button> -->
+  <Button styles="mt-12 mb-24"><a href="https://docs.hyper63.com/getting-started" target="_blank">Get Started</a></Button>
+
+  
   <img src="/all.svg" alt="hyper framework" />
 </section>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,7 +15,7 @@
 </script>
 <script>
   import Header from '$lib/header.svelte'  
-  //import Button from '$lib/button.svelte'
+  import Button from '$lib/button.svelte'
   import Play from '$lib/play-button.svelte'
   import LinkButton from '$lib/link-button.svelte'
   import Ports from '$lib/ports.svelte'
@@ -38,7 +38,8 @@
       <h1 class="text-4xl font-semibold md:text-6xl width-full height-full">Build Your Web Applications at <b>hyper</b> Speed</h1>
       <p class="text-md mt-4 mr-8 md:mt-12 md:w-3/4">hyper provides a full suite of core backend services. Focus on what matters. Stop the bike-shedding. Start building scalable applications today.</p>
       <div class="mt-16 flex space-x-4 md:space-x-20">
-        <LinkButton bgColor="blue" txtColor="white" href="https://docs.hyper63.com/getting-started" target="_blank">Get Started</LinkButton>
+        <Button><a href="https://docs.hyper63.com/getting-started" target="_blank">Get Started</a></Button>
+        
         <Play href="https://blog.hyper63.com/tour-of-hyper63-api/" target="_blank">
           <a href="https://blog.hyper63.com/tour-of-hyper63-api/" target="_blank">Watch</a>
         </Play>
