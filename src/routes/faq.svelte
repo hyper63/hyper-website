99864b13d5a98a53a3389323bf0c2191a044fc62
Tom Wilson <tom@hyper.io>
2022-01-26 15:40:00 +0000

    fix: synced title and h1 and fixed h3 to h2 in header component #38

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -1,66 +1,72 @@
 <script context="module">
-  import { filter, propEq } from 'ramda'
+  import { filter, propEq } from "ramda";
 
-  export async function load({fetch}) {
-    const faqs = await fetch('/admin/faqs.json').then(r => r.json())
-      .then(filter(propEq('active', true)))
+  export async function load({ fetch }) {
+    const faqs = await fetch("/admin/faqs.json")
+      .then((r) => r.json())
+      .then(filter(propEq("active", true)));
     return {
       props: {
-        faqs
-      }
-    }
-
+        faqs,
+      },
+    };
   }
+</script>
 
-</script>
 <script>
-  import Header from '$lib/header.svelte'
-  import Footer from '$lib/footer.svelte'
-  import Search from '$lib/search.svelte'
-  import FAQ from '$lib/faq.svelte'
-  export let faqs
-  export let flags = { search: false, filter: false }
-  
+  import Header from "$lib/header.svelte";
+  import Footer from "$lib/footer.svelte";
+  import Search from "$lib/search.svelte";
+  import FAQ from "$lib/faq.svelte";
+  export let faqs;
+  export let flags = { search: false, filter: false };
 </script>
+
 <svelte:head>
-  <title>FAQ: hyper - service for building hyperscale applications</title>
+  <title>FAQs</title>
 </svelte:head>
 <Header />
 <main>
-  <h1 class="mt-8 mx-auto text-5xl text-center md:text-6xl">FAQs</h1> 
+  <h1 class="mt-8 mx-auto text-5xl text-center md:text-6xl">FAQs</h1>
   <p class="mt-4 px-4 text-base text-darkgray md:text-center">
-  Can’t find the answer you’re looking for? Reach out to our 
-  <a href="https://github.com/hyper63/hyper63/discussions" class="text-red underline">customer support team</a>.
+    Can’t find the answer you’re looking for? Reach out to our
+    <a
+      href="https://github.com/hyper63/hyper63/discussions"
+      class="text-red underline">customer support team</a
+    >.
   </p>
   {#if flags.search}
-  <section class="mt-4 mx-4 md:flex md:justify-center">
-    <Search styles="md:w-2/3" />
-  </section>
+    <section class="mt-4 mx-4 md:flex md:justify-center">
+      <Search styles="md:w-2/3" />
+    </section>
   {/if}
   {#if flags.filter}
-  <section>
-    <h4 class="my-4 text-2xl text-center">Filter By</h4>
-    <div class="mx-4 w-full flex space-x-2 justify-center">
-      <button class="active">All Types</button>
-      <button class="filter">Products</button>
-      <button class="filter">Company</button>
-      <button class="filter">Help</button>
-    </div>
-  </section>
+    <section>
+      <h4 class="my-4 text-2xl text-center">Filter By</h4>
+      <div class="mx-4 w-full flex space-x-2 justify-center">
+        <button class="active">All Types</button>
+        <button class="filter">Products</button>
+        <button class="filter">Company</button>
+        <button class="filter">Help</button>
+      </div>
+    </section>
   {/if}
-  <section class="mx-4 mt-8 py-8 px-4 rounded bg-whitesmoke bg-white md:flex md:justify-center">
+  <section
+    class="mx-4 mt-8 py-8 px-4 rounded bg-whitesmoke bg-white md:flex md:justify-center"
+  >
     <div class="space-y-4 md:w-2/3">
-    {#each faqs as {question, answer}}
-    <FAQ>
-      <span slot="question">{question}</span>
-      <span slot="answer">{@html answer}</span>
-    </FAQ>
-    {/each}
+      {#each faqs as { question, answer }}
+        <FAQ>
+          <span slot="question">{question}</span>
+          <span slot="answer">{@html answer}</span>
+        </FAQ>
+      {/each}
     </div>
   </section>
 </main>
 <hr class="mx-4 border-lightgray" />
 <Footer />
+
 <style lang="postcss">
   .filter {
     @apply font-space border-2 rounded border-gray text-sm bg-whitesmoke text-gray;
@@ -76,6 +82,4 @@
     padding-left: 8px;
     padding-right: 8px;
   }
-
 </style>
-
