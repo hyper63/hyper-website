1add9fb10a1c0b7d0f89217cc4a664bca1317fbf
Tom Wilson <tom@hyper.io>
2022-03-11 21:02:59 +0000

    wip got tooltips working for pricing page

--- src/lib/pricing/compare/scale.svelte
+++ src/lib/pricing/compare/scale.svelte
@@ -1,5 +1,7 @@
 <script>
   import CircleCheck from "$lib/svgs/circle-check.svelte";
+
+  var autoScaleTip = false;
 </script>
 
 <div class="flex items-center">
@@ -31,7 +33,37 @@
     <th>Enterprise</th>
   </tr>
   <tr>
-    <td class="border w-1/5 px-4">Auto-Scaling</td>
+    <td class="border w-1/5 px-4">
+      <div class="flex">
+        <span class="mr-4">Auto-Scaling</span>
+        <span
+          on:mouseenter={() => (autoScaleTip = true)}
+          on:mouseleave={() => (autoScaleTip = false)}
+        >
+          <svg
+            width="13"
+            height="13"
+            viewBox="0 0 13 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12.5 6.5C12.5 8.0913 11.8679 9.61742 10.7426 10.7426C9.61742 11.8679 8.0913 12.5 6.5 12.5C4.9087 12.5 3.38258 11.8679 2.25736 10.7426C1.13214 9.61742 0.5 8.0913 0.5 6.5C0.5 4.9087 1.13214 3.38258 2.25736 2.25736C3.38258 1.13214 4.9087 0.5 6.5 0.5C8.0913 0.5 9.61742 1.13214 10.7426 2.25736C11.8679 3.38258 12.5 4.9087 12.5 6.5ZM7.25 3.5C7.25 3.69891 7.17098 3.88968 7.03033 4.03033C6.88968 4.17098 6.69891 4.25 6.5 4.25C6.30109 4.25 6.11032 4.17098 5.96967 4.03033C5.82902 3.88968 5.75 3.69891 5.75 3.5C5.75 3.30109 5.82902 3.11032 5.96967 2.96967C6.11032 2.82902 6.30109 2.75 6.5 2.75C6.69891 2.75 6.88968 2.82902 7.03033 2.96967C7.17098 3.11032 7.25 3.30109 7.25 3.5ZM5.75 5.75C5.55109 5.75 5.36032 5.82902 5.21967 5.96967C5.07902 6.11032 5 6.30109 5 6.5C5 6.69891 5.07902 6.88968 5.21967 7.03033C5.36032 7.17098 5.55109 7.25 5.75 7.25V9.5C5.75 9.69891 5.82902 9.88968 5.96967 10.0303C6.11032 10.171 6.30109 10.25 6.5 10.25H7.25C7.44891 10.25 7.63968 10.171 7.78033 10.0303C7.92098 9.88968 8 9.69891 8 9.5C8 9.30109 7.92098 9.11032 7.78033 8.96967C7.63968 8.82902 7.44891 8.75 7.25 8.75V6.5C7.25 6.30109 7.17098 6.11032 7.03033 5.96967C6.88968 5.82902 6.69891 5.75 6.5 5.75H5.75Z"
+              fill="#565657"
+            />
+          </svg>
+        </span>
+      </div>
+      {#if autoScaleTip}
+        <div class="absolute z-40 bg-blue text-white p-4 rounded">
+          Ensures instances available to handle
+          <br />
+          the load of your application
+        </div>
+      {/if}
+    </td>
     <td class="border w-1/5">
       <div class="px-16 py-4 flex items-center justify-center">
         <CircleCheck />
--- src/lib/pricing/compare/support.svelte
+++ src/lib/pricing/compare/support.svelte
@@ -1,5 +1,9 @@
 <script>
   import CircleCheck from "$lib/svgs/circle-check.svelte";
+
+  var showBasicTip = false;
+  var showExtTip = false;
+  var showCTOTip = false;
 </script>
 
 <div class="mt-8 flex items-center">
@@ -31,7 +35,36 @@
     <th>Enterprise</th>
   </tr>
   <tr>
-    <td class="border w-1/5 px-4">Basic Support</td>
+    <td class="border w-1/5 px-4">
+      <div class="flex">
+        <span class="mr-4">Basic Support</span>
+        <span
+          on:mouseenter={() => (showBasicTip = true)}
+          on:mouseleave={() => (showBasicTip = false)}
+        >
+          <svg
+            width="13"
+            height="13"
+            viewBox="0 0 13 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12.5 6.5C12.5 8.0913 11.8679 9.61742 10.7426 10.7426C9.61742 11.8679 8.0913 12.5 6.5 12.5C4.9087 12.5 3.38258 11.8679 2.25736 10.7426C1.13214 9.61742 0.5 8.0913 0.5 6.5C0.5 4.9087 1.13214 3.38258 2.25736 2.25736C3.38258 1.13214 4.9087 0.5 6.5 0.5C8.0913 0.5 9.61742 1.13214 10.7426 2.25736C11.8679 3.38258 12.5 4.9087 12.5 6.5ZM7.25 3.5C7.25 3.69891 7.17098 3.88968 7.03033 4.03033C6.88968 4.17098 6.69891 4.25 6.5 4.25C6.30109 4.25 6.11032 4.17098 5.96967 4.03033C5.82902 3.88968 5.75 3.69891 5.75 3.5C5.75 3.30109 5.82902 3.11032 5.96967 2.96967C6.11032 2.82902 6.30109 2.75 6.5 2.75C6.69891 2.75 6.88968 2.82902 7.03033 2.96967C7.17098 3.11032 7.25 3.30109 7.25 3.5ZM5.75 5.75C5.55109 5.75 5.36032 5.82902 5.21967 5.96967C5.07902 6.11032 5 6.30109 5 6.5C5 6.69891 5.07902 6.88968 5.21967 7.03033C5.36032 7.17098 5.55109 7.25 5.75 7.25V9.5C5.75 9.69891 5.82902 9.88968 5.96967 10.0303C6.11032 10.171 6.30109 10.25 6.5 10.25H7.25C7.44891 10.25 7.63968 10.171 7.78033 10.0303C7.92098 9.88968 8 9.69891 8 9.5C8 9.30109 7.92098 9.11032 7.78033 8.96967C7.63968 8.82902 7.44891 8.75 7.25 8.75V6.5C7.25 6.30109 7.17098 6.11032 7.03033 5.96967C6.88968 5.82902 6.69891 5.75 6.5 5.75H5.75Z"
+              fill="#565657"
+            />
+          </svg>
+        </span>
+      </div>
+      {#if showBasicTip}
+        <div class="absolute z-40 bg-blue text-white p-4 rounded">
+          Basic Support available business <br />hourse only, Monday through
+          Friday <br />9 am to 5pm EST
+        </div>
+      {/if}
+    </td>
     <td class="border w-1/5">
       <div class="px-16 py-4 flex items-center justify-center" />
     </td>
@@ -71,7 +104,39 @@
     </td>
   </tr>
   <tr>
-    <td class="border w-1/5 px-4">Extended Support</td>
+    <td class="border w-1/5 px-4">
+      <div class="flex">
+        <span class="mr-4">Extended Support</span>
+        <span
+          on:mouseenter={() => (showExtTip = true)}
+          on:mouseleave={() => (showExtTip = false)}
+        >
+          <svg
+            width="13"
+            height="13"
+            viewBox="0 0 13 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12.5 6.5C12.5 8.0913 11.8679 9.61742 10.7426 10.7426C9.61742 11.8679 8.0913 12.5 6.5 12.5C4.9087 12.5 3.38258 11.8679 2.25736 10.7426C1.13214 9.61742 0.5 8.0913 0.5 6.5C0.5 4.9087 1.13214 3.38258 2.25736 2.25736C3.38258 1.13214 4.9087 0.5 6.5 0.5C8.0913 0.5 9.61742 1.13214 10.7426 2.25736C11.8679 3.38258 12.5 4.9087 12.5 6.5ZM7.25 3.5C7.25 3.69891 7.17098 3.88968 7.03033 4.03033C6.88968 4.17098 6.69891 4.25 6.5 4.25C6.30109 4.25 6.11032 4.17098 5.96967 4.03033C5.82902 3.88968 5.75 3.69891 5.75 3.5C5.75 3.30109 5.82902 3.11032 5.96967 2.96967C6.11032 2.82902 6.30109 2.75 6.5 2.75C6.69891 2.75 6.88968 2.82902 7.03033 2.96967C7.17098 3.11032 7.25 3.30109 7.25 3.5ZM5.75 5.75C5.55109 5.75 5.36032 5.82902 5.21967 5.96967C5.07902 6.11032 5 6.30109 5 6.5C5 6.69891 5.07902 6.88968 5.21967 7.03033C5.36032 7.17098 5.55109 7.25 5.75 7.25V9.5C5.75 9.69891 5.82902 9.88968 5.96967 10.0303C6.11032 10.171 6.30109 10.25 6.5 10.25H7.25C7.44891 10.25 7.63968 10.171 7.78033 10.0303C7.92098 9.88968 8 9.69891 8 9.5C8 9.30109 7.92098 9.11032 7.78033 8.96967C7.63968 8.82902 7.44891 8.75 7.25 8.75V6.5C7.25 6.30109 7.17098 6.11032 7.03033 5.96967C6.88968 5.82902 6.69891 5.75 6.5 5.75H5.75Z"
+              fill="#565657"
+            />
+          </svg>
+        </span>
+      </div>
+      {#if showExtTip}
+        <div class="absolute z-40 bg-blue text-white p-4 rounded">
+          Extended Support available via Zoom
+          <br />
+          business hours only, Monday through
+          <br />
+          Friday 9 am to 5 pm EST
+        </div>
+      {/if}
+    </td>
     <td class="border w-1/5">
       <div class="px-16 py-4 flex items-center justify-center" />
     </td>
@@ -90,7 +155,43 @@
     </td>
   </tr>
   <tr>
-    <td class="border w-1/5 px-4">Fractional CTO Service</td>
+    <td class="border w-1/5 px-4">
+      <div class="flex">
+        <span class="mr-4">Fractional CTO Service</span>
+        <span
+          on:mouseenter={() => (showCTOTip = true)}
+          on:mouseleave={() => (showCTOTip = false)}
+        >
+          <svg
+            width="13"
+            height="13"
+            viewBox="0 0 13 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12.5 6.5C12.5 8.0913 11.8679 9.61742 10.7426 10.7426C9.61742 11.8679 8.0913 12.5 6.5 12.5C4.9087 12.5 3.38258 11.8679 2.25736 10.7426C1.13214 9.61742 0.5 8.0913 0.5 6.5C0.5 4.9087 1.13214 3.38258 2.25736 2.25736C3.38258 1.13214 4.9087 0.5 6.5 0.5C8.0913 0.5 9.61742 1.13214 10.7426 2.25736C11.8679 3.38258 12.5 4.9087 12.5 6.5ZM7.25 3.5C7.25 3.69891 7.17098 3.88968 7.03033 4.03033C6.88968 4.17098 6.69891 4.25 6.5 4.25C6.30109 4.25 6.11032 4.17098 5.96967 4.03033C5.82902 3.88968 5.75 3.69891 5.75 3.5C5.75 3.30109 5.82902 3.11032 5.96967 2.96967C6.11032 2.82902 6.30109 2.75 6.5 2.75C6.69891 2.75 6.88968 2.82902 7.03033 2.96967C7.17098 3.11032 7.25 3.30109 7.25 3.5ZM5.75 5.75C5.55109 5.75 5.36032 5.82902 5.21967 5.96967C5.07902 6.11032 5 6.30109 5 6.5C5 6.69891 5.07902 6.88968 5.21967 7.03033C5.36032 7.17098 5.55109 7.25 5.75 7.25V9.5C5.75 9.69891 5.82902 9.88968 5.96967 10.0303C6.11032 10.171 6.30109 10.25 6.5 10.25H7.25C7.44891 10.25 7.63968 10.171 7.78033 10.0303C7.92098 9.88968 8 9.69891 8 9.5C8 9.30109 7.92098 9.11032 7.78033 8.96967C7.63968 8.82902 7.44891 8.75 7.25 8.75V6.5C7.25 6.30109 7.17098 6.11032 7.03033 5.96967C6.88968 5.82902 6.69891 5.75 6.5 5.75H5.75Z"
+              fill="#565657"
+            />
+          </svg>
+        </span>
+      </div>
+      {#if showCTOTip}
+        <div class="absolute z-40 bg-blue text-white p-4 rounded">
+          Our fractional CTO support provides
+          <br />
+          oversight, vision, and communication
+          <br />
+          to help your organization achieve
+          <br />
+          quality, security, productivity, and
+          <br />
+          accountability.
+        </div>
+      {/if}
+    </td>
     <td class="border w-1/5">
       <div class="px-16 py-4 flex items-center justify-center" />
     </td>
