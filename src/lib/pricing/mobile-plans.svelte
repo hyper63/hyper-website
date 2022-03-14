05f5e21ddcdad922ca7923a7c215946064803365
Tom Wilson <tom@hyper.io>
2022-03-14 13:52:21 +0000

    updated pricing page with mobile break point

---
+++ src/lib/pricing/mobile-plans.svelte
@@ -0,0 +1,56 @@
+<section class="space-y-4">
+  <article class="mx-4 border-3 border-lightgray px-4 py-4 rounded-lg">
+    <header>
+      <a
+        class="px-4 py-2 rounded bg-darkgray text-white float-right"
+        href="https://dashboard.hyper.io">Sign up</a
+      >
+      <h3 class="text-xl">Starter</h3>
+      <h5 class="text-lg text-black font-bold">Free</h5>
+    </header>
+    <p class="mt-8 text-base text-darkgray">
+      For prototyping and students, you get 3 hyper application services, and
+      community support.
+    </p>
+  </article>
+  <article class="mx-4 border-3 border-lightgray px-4 py-4 rounded-lg">
+    <header>
+      <a
+        class="px-4 py-2 rounded bg-blue text-white float-right"
+        href="https://dashboard.hyper.io/upgrade">Upgrade</a
+      >
+      <h3 class="text-xl">Pro</h3>
+      <h5 class="text-lg text-black font-bold">$99/mo</h5>
+    </header>
+    <p class="mt-8 text-base text-darkgray">
+      For startups, 10 hyper application services, 5 users, and email support.
+    </p>
+  </article>
+  <article class="mx-4 border-3 border-lightgray px-4 py-4 rounded-lg">
+    <header>
+      <a
+        class="px-4 py-2 rounded bg-green text-white float-right"
+        href="https://dashboard.hyper.io/upgrade">Upgrade</a
+      >
+      <h3 class="text-xl">Business</h3>
+      <h5 class="text-lg text-black font-bold">$949/mo</h5>
+    </header>
+    <p class="mt-8 text-base text-darkgray">
+      For agencies, 20 hyper application services, 15 users, email support,
+      99.9% SLA uptime.
+    </p>
+  </article>
+  <article class="mx-4 border-3 border-lightgray px-4 py-4 rounded-lg">
+    <header>
+      <a class="px-4 py-2 rounded bg-purple text-white float-right" href="#"
+        >Contact</a
+      >
+      <h3 class="text-xl">Enterprise</h3>
+      <h5 class="text-lg text-black font-bold">Custom</h5>
+    </header>
+    <p class="mt-8 text-base text-darkgray">
+      For agencies, 20 hyper application services, 15 users, email support,
+      99.9% SLA uptime.
+    </p>
+  </article>
+</section>
