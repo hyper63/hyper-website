efb71b37743c070192b6b8f0dc057ef8cd88446f
tripott <tripott@gmail.com>
2022-03-25 20:01:53 +0000

    header menu and dashboard button

--- src/lib/nav-button.svelte
+++ src/lib/nav-button.svelte
@@ -2,22 +2,23 @@
   export let txtColor = "blue";
   export let bgColor = "lightblue";
   export let hoverTextColor = "white";
+  export let hoverBackgroundColor = "white";
 </script>
 
 <button
   on:click
-  class="text-{txtColor} bg-{bgColor} hover:bg-{txtColor} hover:text-{hoverTextColor}"
+  class="text-{txtColor} bg-{bgColor} hover:bg-{hoverBackgroundColor} hover:text-{hoverTextColor}"
 >
   <slot />
 </button>
 
 <style lang="postcss">
   button {
-    @apply rounded-sm inline justify-center;
+    @apply rounded-lg inline justify-center;
     font-size: 14px;
     height: 38px;
-    width: 89px;
+    width: 100px;
     margin: 0;
-    padding: 0;
+    padding: 4px;
   }
 </style>
