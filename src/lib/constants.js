ca3ef08fab6b88f52e6b3d855f9ea1b6d98db75c
tripott <tripott@gmail.com>
2022-04-13 18:50:38 +0000

    feat(website) new homepage getting started video

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -13,5 +13,5 @@ export const HelpURLs = {
   SEARCH_API: "https://docs.hyper.io/search-api",
   HYPER_CONNECT: "https://docs.hyper.io/hyper-connect",
   SLACK_SUPPORT: "https://hyper-chat.slack.com/archives/C023YLD15NZ",
-  VIDEO_PROMO: "https://youtu.be/PJ-WV9yPyOo",
+  VIDEO_PROMO: "https://youtu.be/E_-MhLPSOXU",
 };
