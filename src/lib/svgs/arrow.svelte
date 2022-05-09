72dcf32d4885f08a43d6440297b221266e6676b7
Tyler Hall <tyler@hyper.io>
2022-05-09 19:30:23 +0000

    feat: add learn more section

--- src/lib/svgs/arrow.svelte
+++ src/lib/svgs/arrow.svelte
@@ -1,7 +1,11 @@
+<script>
+  export let color = "currentColor";
+</script>
+
 <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path
     d="M8 1L13 6M13 6L8 11M13 6H1"
-    stroke="white"
+    stroke={color}
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
